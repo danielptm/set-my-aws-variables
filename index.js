d625a39080f69bb77a5e300ad050487190663762
Daniel <daniel.tuttle@nike.com>
2019-04-16 15:21:36 -0700

    Got it working to extract the aws credentials from the .aws folder

--- index.js
+++ index.js
@@ -2,41 +2,61 @@ const fs = require("fs");
 const basePath = "/Users";
 const readline = require("linebyline");
 
+const AWS_ACCESS_KEY_ID = "aws_access_key_id";
+const AWS_SECRET_ACCESS_KEY = "aws_secret_access_key";
+const AWS_SESSION_TOKEN = "aws_session_token";
+
+let aws_access_key_id = "";
+let aws_secret_access_key = "";
+let aws_session_token = "";
+
 getAwsCredentialsPath()
 .then(function(credentialsPath) {
   getContentsFromCredentialsFile(credentialsPath)
   .then((content) => {
-    const aws_access_key_id = "";
-    const aws_secret_access_key = "";
-    const aws_session_token = "";
-    const rl = readline(credentialsPath);
+    let rl = readline(credentialsPath);
     rl.on('line', function(line, linecount, bytecount) {
-      if(line.indexOf("aws_access_key_id") > -1) {
-        if(aws_access_key_id != "") {
-          aws_access_key_id = getAwsAccessKeyId(line);
-        } else if (line.indexOf("aws_secret_access_key") > -1) {
-          if (aws_secret_access_key == "") {
-            aws_secret_access_key = getSecretAccessKeyId(line);
-          }
-        } else if (aws_session_token == "") {
-          aws_session_token = getSessionToken(line);
+      if(line.indexOf(AWS_ACCESS_KEY_ID) > -1) {
+        if (aws_access_key_id != "") {
+          aws_access_key_id = getAccessKey(line);
+        }
+      } if (line.indexOf(AWS_SECRET_ACCESS_KEY) > -1) {
+        if (aws_secret_access_key == "") {
+          aws_secret_access_key = getAccessKey(line);
+        }
+      } if (line.indexOf(AWS_SESSION_TOKEN) > -1) {
+        if (aws_session_token == "") {
+          aws_session_token = getAccessKey(line);
         }
       }
     })
+
+    process.env.AWS_ACCESS_KEY_ID = aws_access_key_id;
+    process.env.AWS_SECRET_ACCESS_KEY = aws_secret_access_key;
+    process.env.AWS_SESSION_TOKEN = aws_session_token;
+    process.env.DAN = "ME";
+
   });
 });
 
-function getAwsAccessKeyId(content) {
-  return ;
+function getAccessKey(content) {
+  let beg = content.indexOf(" = ") + 3;
+  let returnString = content.substring(beg, content.length);
+  console.log("*");
+  console.log(returnString);
+  return returnString;
 }
 
-function getSecretAccessKeyId(content){
-
-}
-
-function getSessionToken() {
-
-}
+// function getSecretAccessKeyId(content){
+//   let beg = content.indexOf("=") + 3;
+//   let returnString = content.substring(beg, content.length);
+//   console.log(returnString);
+//   return returnString;
+// }
+//
+// function getSessionToken() {
+//
+// }
 
 function getContentsFromCredentialsFile(path) {
   console.log(path);
